0ce04fda1ee06f0682d1491365aecff73d3ef774
amitjhacse <akj.cs.20@gmail.com>
2020-10-28 22:59:42 +0530

    static file serve express

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(morgan("dev"));
+
+//  serve-static files
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   req.requestTime = new Date().toLocaleString();
   next();
